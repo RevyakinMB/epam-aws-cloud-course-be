c19d54b0141548dbdcb21336306d79603e510895
Maxim Revyakin <Maxim_Revyakin@epam.com>
2022-07-15 22:41:08 +0400

    Provide an option to skip data provider

--- products-service/src/functions/get-products-list/handler.ts
+++ products-service/src/functions/get-products-list/handler.ts
@@ -4,7 +4,22 @@ import { getMockedProducts } from '@api/index';
 import { formatJSONResponse, formatJSONErrorResponse } from '@libs/api-gateway';
 import { Product } from '@ptypes/product';
 
-export const getProductsList: APIGatewayProxyHandler = async () => {
+const defaultProducts: Product[] = [
+  { id: 1, name: 'Product 1', description: 'Description 1', price: 1 },
+  { id: 2, name: 'Product 2', description: 'Description 2', price: 2 },
+  { id: 3, name: 'Product 3', description: 'Description 3', price: 3 },
+];
+
+export const getProductsList: APIGatewayProxyHandler = async (event) => {
+  // an escape hatch for the case when dummy data provider refuses to work
+  const { skipDataProvider } = event.queryStringParameters || {};
+  if (skipDataProvider) {
+    return formatJSONResponse({
+      data: defaultProducts,
+      count: defaultProducts.length,
+    });
+  }
+
   let products: Product[];
   try {
     products = await getMockedProducts();
