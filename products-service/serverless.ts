bba11478cc71051a4ca8049ce81751629e8033ac
Maxim Revyakin <Maxim_Revyakin@epam.com>
2022-07-17 00:26:12 +0400

    Add Swagger bucket

--- products-service/serverless.ts
+++ products-service/serverless.ts
@@ -6,7 +6,10 @@ import getProductById from '@functions/get-product-by-id';
 const serverlessConfiguration: AWS = {
   service: 'products-service',
   frameworkVersion: '3',
-  plugins: ['serverless-esbuild'],
+  plugins: [
+    'serverless-esbuild',
+    'serverless-plugin-swagger-ui',
+  ],
   provider: {
     name: 'aws',
     runtime: 'nodejs14.x',
@@ -37,6 +40,61 @@ const serverlessConfiguration: AWS = {
       define: { 'require.resolve': undefined },
       platform: 'node',
       concurrency: 10,
+    },
+    swaggerUi: {
+      s3Bucket: 'products-service-api-swagger',
+      exportType: 'oas30',
+      accepts: 'application/yaml',
+      extensions: 'integrations',
+      swaggerUiDirectoryName: '.swagger-ui',
+      swaggerUiConfig: {
+        dom_id: '#swagger-ui',
+        deepLinking: true,
+        presets: [
+          'SwaggerUIBundle.presets.apis',
+          'SwaggerUIStandalonePreset',
+        ],
+        plugins: [
+          'SwaggerUIBundle.plugins.DownloadUrl',
+        ],
+        layout: [
+          'StandaloneLayout',
+        ],
+      },
+    },
+  },
+  resources: {
+    Resources: {
+      S3BucketApiDocs: {
+        Type: 'AWS::S3::Bucket',
+        Properties: {
+          BucketName: '${self:custom.swaggerUi.s3Bucket}',
+          AccessControl: 'PublicRead',
+          WebsiteConfiguration: {
+            IndexDocument: 'index.html',
+          },
+        },
+      },
+      S3BucketPolicyApiDocs: {
+        Type: 'AWS::S3::BucketPolicy',
+        Properties: {
+          Bucket: {
+            Ref: 'S3BucketApiDocs',
+          },
+          PolicyDocument: {
+            Statement: [{
+              Action: [
+                's3:GetObject',
+              ],
+              Effect: 'Allow',
+              Resource: {
+                'Fn::Sub': 'arn:aws:s3:::${S3BucketApiDocs}/*',
+              },
+              Principal: '*',
+            }],
+          },
+        },
+      },
     },
   },
 };
