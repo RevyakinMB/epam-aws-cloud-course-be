12852760c7758372c609a2cf5c2af2529c51f1b2
Maxim Revyakin <Maxim_Revyakin@epam.com>
2022-07-23 15:00:22 +0400

    Replace docs; Add pg connection settings

--- products-service/serverless.ts
+++ products-service/serverless.ts
@@ -3,6 +3,8 @@ import type { AWS } from '@serverless/typescript';
 import getProductsList from '@functions/get-products-list';
 import getProductById from '@functions/get-product-by-id';
 
+import { serverlessDbEnvConfiguration } from './env.serverless';
+
 const serverlessConfiguration: AWS = {
   service: 'products-service',
   frameworkVersion: '3',
@@ -22,6 +24,7 @@ const serverlessConfiguration: AWS = {
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+      ...serverlessDbEnvConfiguration,
     },
   },
   // import the function via paths
