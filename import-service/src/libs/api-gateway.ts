9ef619e8930e43aa733f6673c7b769c8eb134b6b
Maxim Revyakin <Maxim_Revyakin@epam.com>
2022-07-30 14:43:21 +0400

    Add product importing project

---
+++ import-service/src/libs/api-gateway.ts
@@ -0,0 +1,30 @@
+import type { APIGatewayProxyEvent, APIGatewayProxyResult, Handler } from 'aws-lambda';
+import type { FromSchema } from 'json-schema-to-ts';
+
+type ValidatedAPIGatewayProxyEvent<S> = Omit<APIGatewayProxyEvent, 'body'> & { body: FromSchema<S> }
+export type ValidatedEventAPIGatewayProxyEvent<S> = Handler<
+  ValidatedAPIGatewayProxyEvent<S>,
+  APIGatewayProxyResult
+>;
+
+const CORS_HEADERS = {
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': '*',
+  },
+};
+
+export const formatJSONResponse = (response: Record<string, unknown>, statusCode = 200) => ({
+  body: JSON.stringify(response),
+  ...CORS_HEADERS,
+  statusCode,
+});
+
+export const formatJSONErrorResponse = (
+  statusCode: number,
+  message: string,
+) => ({
+  body: JSON.stringify({ message }),
+  ...CORS_HEADERS,
+  statusCode,
+});
