eb91941326dde08ab7e853028581317941942bcf
Maxim Revyakin <Maxim_Revyakin@epam.com>
2022-08-15 23:37:07 +0400

    cleanup comments

--- authorization-service/src/functions/basic-authorizer/index.ts
+++ authorization-service/src/functions/basic-authorizer/index.ts
@@ -2,12 +2,4 @@ import { handlerPath } from '@libs/handler-resolver';
 
 export default {
   handler: `${handlerPath(__dirname)}/handler.basicAuthorizer`,
-  // events: [{
-  //   http: {
-  //     path: 'token',
-  //     method: 'get',
-  //     cors: true,
-  //     authorizer: {},
-  //   },
-  // }],
 };
